eb4775c22e8997ca2dd37d391ff44906416184a5
Javiera Guerrero <javiera.guerrero@@soymas.cl>
2023-08-07 14:12:21 -0400

    recur

--- recursion.py
+++ recursion.py
@@ -5,4 +5,21 @@ def countdown(num):
     print(num)
     countdown(num - 1) 
 
-countdown(5)           
+countdown(5)    
+
+print("---------------")
+
+def countdown2(num):
+  for number in range(-num, 0):
+      print(-number)    
+
+  print("caboom")
+
+countdown2(5)
+
+print("---------------")
+
+def infinite_recursion(word):
+  print(word)
+  infinite_recursion(word)
+infinite_recursion("miaumiau")      
